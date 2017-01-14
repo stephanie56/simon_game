d47087b718fb6ca65a170c30d2ce12ae24f17c94
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-01-14 17:04:13 -0500

    Updated activeBlock function

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ $(document).ready(function(){
   var pattern = [1,4,2,3,4,2,2,1,3,1,2,4,1,3,2,1,3,4,2,2];
   var num = pattern[0];
   $('#start').click(function(){
-  setInterval(changeColor, 5000);
+    activeBlock(1, 1000);
+    activeBlock(4, 3000);
+    activeBlock(2, 7000);
+    activeBlock(4, 10000);
+    activeBlock(2, 13000);
+    activeBlock(2, 16000);
+    activeBlock(1, 19000);
+    
     console.log("im changing color!");
   });
 
-  function changeColor (){
-    $('[data-key="1"]').css('opacity','0.6');
+
+  function activeBlock (step, timeOut){
+    setTimeout(function() { changeColor(step) }, timeOut);
+    console.log("pressing " + step);
+  }
+
+  function changeColor (num){
+    $('[data-key="'+ num + '"]').css('opacity','0.6');
+    setTimeout(function(){
+      $('[data-key="'+ num  +'"]').css('opacity','1.0');
+    }, 2000)
   }
 
   //console.log(pattern.length);
